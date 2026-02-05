ea3b4679b5d9a16450d05c67f6ef224ec48ac9aa
abhigyan1903 <abhigyanchandrakar@gmail.com>
2026-02-05 15:57:06 +0530

    a.c.p_sets.py homework done

---
+++ sets and array/a.c.p_sets.py
@@ -0,0 +1,10 @@
+#program to find the symmetric difference of two different sets
+set1={1,2,3,4,5,6,7,8,9}
+set2={0,1,3,7,9,11}
+
+print("the symmeric difference of these two sets is :", set1.symmetric_difference(set2))
+
+set_1={'blue','red','green'}
+set_2={'red','yellow','green'}
+
+print("The Symmeric Difference of these two sets is :", set_1.symmetric_difference(set_2))
