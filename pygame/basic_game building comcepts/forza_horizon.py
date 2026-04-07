65a57fd93ce72c18829ecc643a046b862705c5f5
abhigyan1903 <abhigyanchandrakar@gmail.com>
2026-04-07 16:56:01 +0530

    space invader project

--- pygame/basic_game building comcepts/forza_horizon.py
+++ pygame/basic_game building comcepts/forza_horizon.py
@@ -8,19 +8,20 @@ display_1=p.display.set_mode((Screen_width,Screen_height))
 p.display.set_caption("Forza Horizon 7")\
 
 bg_image=p.transform.scale(p.image.load("car.png").convert(), (Screen_width,Screen_height))     
-sprite=p.transform.scale(p.image.load("car selection.png").convert_alpha(),(100,100))
+sprite=p.transform.scale(p.image.load("car selection.png").convert_alpha(),(500,200))
 sprite_rect=sprite.get_rect(center=(Screen_width//2,Screen_height//2+200))
 text=p.font.Font(None,30).render("Forza Horizon 7",True,p.Color("white"))
 text_rect=text.get_rect(center=(Screen_width//2,Screen_height//2-200))
 
 #main loop
-running_1=True
-clock=p.time.Clock()
-def game_loop():
- while running_1:
+def game_Loop():
+ running=True
+ clock=p.time.Clock()
+
+ while running:
     for event in p.event.get():
         if event.type==p.QUIT:
-            running_1=False
+            running=False
     display_1.blit(bg_image,(0,0))
     display_1.blit(sprite,sprite_rect)
     display_1.blit(text,text_rect)
@@ -30,4 +31,4 @@ def game_loop():
  p.quit()
  
 if __name__=="__main__":
-    game_loop()
+    game_Loop()
--- pygame/space_invader/space_invader.py
+++ pygame/space_invader/space_invader.py
@@ -2,9 +2,10 @@
 import pygame as p 
 import random as r
 import math as m 
+
 #constant
 
-Screen_width,Screen_height=1000,800
+Screen_width,Screen_height=800,500
 player_start_x=370
 player_start_y=380
 enemy_start_y_min=50
@@ -12,7 +13,7 @@ enemy_start_y_max=150
 enemy_speed_x=4
 enemy_speed_y=40
 bullet_speed_y=10
-collision_distance=2
+collision_distance=27
 #initialization
 p.init()
 screen=p.display.set_mode((Screen_width,Screen_height))
@@ -50,7 +51,31 @@ bulletx_change=0
 bullety_change=bullet_speed_y
 bullet_state="ready"
 
-
+# lets add the score
+score_value=0
+font=p.font.Font(None,32)
+text_x=10
+text_y=10
+over_font=p.font.Font(None,64)
+def show_score(x,y):
+  #displaying the score
+  score=font.render("score: " + str(score_value),True,(255,255,255) )
+  screen.blit(score,(x,y))
+def game_over_text():
+    over_text=over_font.render("GAME OVER",True,(255,255,255))
+    screen.blit(over_text,(200,250))
+def player( x,y):
+    screen.blit(player_img,(x,y))
+def enemy(x,y,i):
+    screen.blit(enemy_image[i],(x,y))
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state="fire"
+    screen.blit(bullet_img,(x+16,y+10))
+def collision(enemyx,enemyy,bulletx,bullety):
+    distance=m.sqrt((enemyx-bulletx)**2 + (enemyy-bullety)**2)
+    return distance < collision_distance
+         
 running=True
 while running:
     screen.fill((0,0,0))
@@ -58,9 +83,55 @@ while running:
     for event in p.event.get():
         if event.type== p.QUIT:
             running=False
+        if event.type==p.KEYDOWN:
+            if event.key==p.K_LEFT:
+                playerx_change=-5
+            if event.key==p.K_RIGHT:
+                playerx_change=5
+            if event.key==p.K_SPACE and bullet_state=="ready":
+                bulletx=playerx
+                fire_bullet(bulletx,bullety)
+        if event.type==p.KEYUP and (event.key in [p.K_LEFT,p.K_RIGHT]):
+            playerx_change=0
+    playerx+=playerx_change 
+    playerx=max(0,min(playerx,Screen_width-64))
+    
+    for i in range(num_of_enemies):
+        if enemyy[i]>340:
+            for j in range(num_of_enemies):
+                enemyy[j]=2000
+            game_over_text()
+            break
+        
+        enemyx[i]+=enemyx_change[i]
+        if enemyx[i]<=0 or enemyx[i]>=Screen_width-64:
+            enemyx_change[i]*=-1
+            enemyy[i]+=enemyy_change[i]
+        
+        if collision(enemyx[i],enemyy[i],bulletx,bullety):
+              bullety=player_start_y
+              bullet_state="ready"
+              score_value+=1
+              enemyx[i]=r.randint(0,Screen_width-64)
+              enemyy[i]=r.randint(enemy_start_y_min,enemy_start_y_max)
+        
+        enemy(enemyx[i],enemyy[i],i)
+        
+    if bullety<=0:
+        bullety=player_start_y
+        bullet_state="ready"  
+    elif bullet_state=="fire":
+        fire_bullet(bulletx,bullety)
+        bullety-=bullety_change
+        
+        
+    player(playerx,playery)
+    show_score(text_x,text_y) 
+    p.display.update()   
+            
+            
         
         
         
         
-        
-    p.display.update()
+
